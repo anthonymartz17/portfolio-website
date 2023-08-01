49056c77940249758806dac06e3b64500629d323
Antonio <anthonymartz17@hotmail.com>
2023-08-01 13:24:11 -0400

    working on adding images for thumbnail to firebase storage

--- src/views/blog/admin-app.vue
+++ src/views/blog/admin-app.vue
@@ -3,18 +3,31 @@ import { quillEditor } from "vue-quill-editor";
 import { required } from "vuelidate/lib/validators";
 import { mapActions } from "vuex";
 import BaseButton from "@/components/baseButton.vue";
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
 
 export default {
 	components: {
 		quillEditor,
 		BaseButton,
+		vue2Dropzone,
 	},
 	data() {
 		return {
-			isSubmitted: false,
+			submitted: false,
 			postId: null,
 			blog: {},
-			blogThumbnail: {},
+			blogThumbnail: [],
+			dropzoneOptions: {
+				url: "https://httpbin.org/post",
+				thumbnailWidth: 150,
+				thumbnailHeight: 150,
+				maxFiles: 1,
+				acceptedFiles: ".jpg, .jpeg, .png",
+				headers: { "My-Awesome-Header": "header value" },
+				dictDefaultMessage: "<i class='fa fa-cloud-upload'></i>UPLOAD IMAGE",
+				addRemoveLinks: true,
+			},
 		};
 	},
 	validations: {
@@ -22,7 +35,9 @@ export default {
 			title: { required },
 			content: { required },
 		},
-		// blogThumbnail: { required },
+		blogThumbnail: {
+			required,
+		},
 	},
 	created() {
 		this.postId = this.$route.query.postId;
@@ -33,10 +48,28 @@ export default {
 		}
 	},
 	methods: {
-		...mapActions("blogPosts", ["fetchPostById", "createPost","updatePost"]),
+		...mapActions("blogPosts", ["fetchPostById", "createPost", "updatePost"]),
+		afterComplete(file) {
+			this.blogThumbnail.push(file);
+			console.log(...this.blogThumbnail)
+		},
+		removeImg(file) {
+			this.blogThumbnail.filter((img) => img.dataURL != file.dataURL)
+			console.log(...this.blogThumbnail)
+		},
+		async uploadImgManually() {
+			const thumb = Object.values(this.blogThumbnail);
+			if (thumb.length > 0) {
+				//add images to vuedropzone
+				this.$refs.myVueDropzone.manuallyAddFile(
+					this.blogThumbnail,
+					this.blogThumbnail.dataURL
+				);
+			}
+		},
 		async tryCreatePost() {
 			this.$v.$touch;
-			this.isSubmitted = true;
+			this.submitted = true;
 			if (this.$v.$invalid) {
 				return;
 			} else {
@@ -45,17 +78,20 @@ export default {
 						const response = await this.updatePost(this.blog);
 						this.blog = {};
 						this.blogThumbnail = {};
-						this.isSubmitted = false;
+						this.submitted = false;
 						this.$router.push({ name: "home-blog" });
 					} catch (error) {
 						throw error;
 					}
 				} else {
 					try {
-						const response = await this.createPost(this.blog);
+						const response = await this.createPost({
+							post: this.blog,
+							thumbnail: this.blogThumbnail[0],
+						});
 						this.blog = {};
-						this.blogThumbnail = {};
-						this.isSubmitted = false;
+						this.blogThumbnail = [];
+						this.submitted = false;
 						this.$router.push({ name: "home-blog" });
 					} catch (error) {
 						throw error;
@@ -75,6 +111,24 @@ export default {
 	>
 		<h2 class="blog-list-title">Create New Post</h2>
 		<form action="" @submit.prevent="tryCreatePost">
+			<div>
+				<label class="label-field" for="title">Thumbnail</label>
+				<vue2Dropzone
+					@vdropzone-removed-file="removeImg"
+					@vdropzone-mounted="uploadImgManually"
+					@vdropzone-complete="afterComplete"
+					:destroyDropzone="false"
+					ref="myVueDropzone"
+					id="dropzone"
+					:options="dropzoneOptions"
+					:class="{
+						invalid: submitted && $v.blogThumbnail.$invalid,
+					}"
+				></vue2Dropzone>
+				<div v-if="submitted && !$v.blogThumbnail.required" class="text-small">
+					Photos are required.
+				</div>
+			</div>
 			<div class="field">
 				<label class="label-field" for="title">Title</label>
 				<input
@@ -83,10 +137,10 @@ export default {
 					id="title"
 					:class="[
 						'input-field',
-						{ invalid: isSubmitted && $v.blog.title.$invalid },
+						{ invalid: submitted && $v.blog.title.$invalid },
 					]"
 				/>
-				<p v-if="isSubmitted && !$v.blog.title.required" class="text-small">
+				<p v-if="submitted && !$v.blog.title.required" class="text-small">
 					Please enter a title
 				</p>
 			</div>
@@ -94,11 +148,11 @@ export default {
 			<div>
 				<label class="label-field" for="content">Content</label>
 				<quill-editor
-					:class="{ invalid: isSubmitted && $v.blog.content.$invalid }"
+					:class="{ invalid: submitted && $v.blog.content.$invalid }"
 					v-model="blog.content"
 					ref="quillEditor"
 				></quill-editor>
-				<p v-if="isSubmitted && !$v.blog.content.required" class="text-small">
+				<p v-if="submitted && !$v.blog.content.required" class="text-small">
 					Content can not be empty
 				</p>
 			</div>
@@ -113,9 +167,14 @@ export default {
 		</form>
 	</div>
 </template>
-
 <style lang="scss" scoped>
 /* Custom CSS to display images inline */
+#dropzone {
+	background: none;
+	border: 1px solid rgba($white, 0.3);
+	margin-bottom: 1em;
+}
+
 .field {
 	margin-bottom: 2em;
 }

728abaf605f408d6aaf6193d31382fc38585f42a
Antonio <anthonymartz17@hotmail.com>
2023-08-08 10:48:40 -0400

    added resume download functionality

---
+++ src/views/resume-page.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="pdf-view">
+		<object
+			data="/my-CV/Antonio_Martinez_CV.pdf"
+			type="application/pdf"
+			width="100%"
+			height="600px"
+		>
+			<div class="noPdfView">
+				<p>
+					Your browser does not support PDF viewing. You can download the resume here.
+				</p>
+				<ResumeDownload class="button" />
+			</div>
+		</object>
+	</div>
+</template>
+
+<script>
+import ResumeDownload from "../components/buttons/resume-download.vue";
+export default {
+	components: { ResumeDownload },
+};
+</script>
+
+<style lang="scss" scoped>
+.pdf-view {
+	display: flex;
+  justify-content: center;
+  align-items: center;
+	height: 100%;
+
+	div {
+		color: $white;
+		font: $font-text-mb;
+		display: flex;
+    flex-direction: column;
+    align-items: center;
+		text-align: center;
+    padding: 1em;
+	}
+	.button {
+		width: 30em;
+		margin-block: 2em;
+	}
+}
+</style>

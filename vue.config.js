77e0cdd86464d30dae1d49702fdaab32819f2130
Antonio <anthonymartz17@hotmail.com>
2023-08-21 18:56:09 -0400

    updated babel.config to add plugins: [@babel/plugin-transform-private-methods]

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,24 @@
-const {defineConfig} = require("@vue/cli-service");
-module.exports = defineConfig({
-	transpileDependencies: true,
-});
+const { defineConfig } = require("@vue/cli-service");
 
-module.exports = {
-	css: {
-		loaderOptions: {
-			sass: {
-				additionalData: `
-        @import "@/assets/scss/_variables.scss";
-        @import "@/assets/scss/_functions.scss";
-        @import "@/assets/scss/_mixins.scss";
-        @import "@/assets/scss/_classes.scss";
-             
+module.exports = defineConfig({
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/assets/scss/_variables.scss";
+          @import "@/assets/scss/_functions.scss";
+          @import "@/assets/scss/_mixins.scss";
+          @import "@/assets/scss/_classes.scss";
         `,
-			},
-		},
-	},
-};
+      },
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': require('path').resolve(__dirname, 'src'),
+      },
+    },
+  },
+});

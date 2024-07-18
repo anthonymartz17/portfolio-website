5750bbc60ec40d8c9d84b99a61271d92c03b8f6f
antonio <antonio.fr.martinezc@hotmail.com>
2024-07-18 11:03:30 -0400

    updated firebase config file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+	apiKey: process.env.VUE_APP_API_KEY,
+	authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+	databaseURL: process.env.VUE_APP_DATABASE_URL,
+	projectId: process.env.VUE_APP_PROJECT_ID,
+	storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+	appId: process.env.VUE_APP_APP_ID,
+	measurementId: process.env.VUE_APP_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const storage = getStorage(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+export { auth, db, storage };

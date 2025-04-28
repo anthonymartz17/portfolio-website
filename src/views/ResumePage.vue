a01d5f60f8dbe9a344381a1b3dcb61fc9b62d0f2
antonio <antonio.fr.martinezc@hotmail.com>
2025-04-28 19:19:27 -0400

    updated ResumePage to show resume directly from firebase

--- src/views/ResumePage.vue
+++ src/views/ResumePage.vue
@@ -1,14 +1,15 @@
 <template>
 	<div class="pdf-view">
 		<object
-			data="/my-CV/Antonio_Martinez_CV.pdf"
+			data="https://firebasestorage.googleapis.com/v0/b/portfolio-2023-166e2.appspot.com/o/pdfs%2Fantoniomartinez_cv_resume.pdf?alt=media&token=abc73407-4a80-4efd-a37c-682fc1bd8a21"
 			type="application/pdf"
 			width="100%"
 			height="600px"
 		>
 			<div class="noPdfView">
 				<p>
-					Your browser does not support PDF viewing. You can download the resume here.
+					Your browser does not support PDF viewing. You can download the resume
+					here.
 				</p>
 				<ResumeDownload class="button" />
 			</div>
@@ -26,18 +27,18 @@ export default {
 <style lang="scss" scoped>
 .pdf-view {
 	display: flex;
-  justify-content: center;
-  align-items: center;
+	justify-content: center;
+	align-items: center;
 	height: 100%;
 
 	div {
 		color: $white;
 		font: $font-text-mb;
 		display: flex;
-    flex-direction: column;
-    align-items: center;
+		flex-direction: column;
+		align-items: center;
 		text-align: center;
-    padding: 1em;
+		padding: 1em;
 	}
 	.button {
 		width: 30em;
